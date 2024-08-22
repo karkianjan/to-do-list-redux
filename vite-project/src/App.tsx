f3900c10ba918fbdf6b954def4210f4897db946f
karkianjan <carkey.anjan2018@gmail.com>
2024-08-22 13:28:20 +0545

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>To-Do-List-Redux</p>
+    </>
+  )
+}
+
+export default App
